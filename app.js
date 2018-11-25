f2d57bca2ae794cacb2701a99124c7c9b7741412
MULIER <brian.mulier@cgi.com>
2018-11-25 01:17:40 +0100

    Game with prediction (add input to server's update)

---
+++ app.js
@@ -0,0 +1,86 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var id = 1;
+
+// Chargement du fichier index.html affiché au client
+var server = http.createServer(function (req, res) {
+    var fileStream;
+    var resourcePath;
+    if (req.url === "/") {
+        fs.readFile('./index.html', 'utf-8', function (error, content) {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(content);
+        });
+    } else if (req.url.match("\.css$")) {
+        resourcePath = path.join(__dirname, req.url);
+        fileStream = fs.createReadStream(resourcePath, "UTF-8");
+        res.writeHead(200, {"Content-Type": "text/css"});
+        fileStream.pipe(res);
+    } else if (req.url.match("\.png$")) {
+        resourcePath = path.join(__dirname, req.url);
+        fileStream = fs.createReadStream(resourcePath);
+        res.writeHead(200, {"Content-Type": "image/png"});
+        fileStream.pipe(res);
+    } else if (req.url.match("\.js$")) {
+        resourcePath = path.join(__dirname, req.url);
+        fileStream = fs.createReadStream(resourcePath, "UTF-8");
+        res.writeHead(200, {"Content-Type": "application/javascript"});
+        fileStream.pipe(res);
+    } else {
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end("No Page Found");
+    }
+});
+
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+
+var players = {};
+var inputs = [];
+
+var SPEED = 150;
+var WORLD = {
+    width: 400,
+    height: 400
+};
+var SQUARESIZE = 50;
+
+// Quand un client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket) {
+    console.log('Un client est connecté !');
+
+    socket.on('disconnect', function () {
+        delete players[socket.id];
+        console.log('Un client s\'est déconnecté !');
+    });
+
+    socket.on('player_update', function (input) {
+        input.id = socket.id;
+        inputs.push(input);
+    });
+
+    players[socket.id] = {
+        x: Math.round(Math.random() * (WORLD.width - SQUARESIZE)),
+        y: Math.round(Math.random() * (WORLD.height - SQUARESIZE))
+    };
+
+    setInterval(function () {
+        simulateWorld(Date.now());
+    }, 300);
+});
+
+function simulateWorld() {
+    for(var idx = 0; idx < inputs.length; idx++){
+        var input = inputs[idx];
+        var player = players[input.id];
+        player.x += input.vx * SPEED * input.elapsedTime;
+        player.y += input.vy * SPEED * input.elapsedTime;
+    };
+    io.sockets.emit("game_update", {timestamp: Date.now(), players: players, inputs: inputs});
+    inputs = [];
+}
+
+server.listen(8080, "0.0.0.0");
+
+console.log("Server created on localhost:8080")
---
+++ js/game.js
@@ -0,0 +1,120 @@
+var DIRECTIONS = [{code: 'S'.charCodeAt(0), dir: {x: 0, y: 1}}, {
+    code: 'Z'.charCodeAt(0),
+    dir: {x: 0, y: -1}
+}, {code: 'D'.charCodeAt(0), dir: {x: 1, y: 0}}, {code: 'Q'.charCodeAt(0), dir: {x: -1, y: 0}}]
+
+var player;
+
+var otherPlayers = {};
+
+var SQUARESIZE = 50;
+
+var SPEED = 150;
+
+var keyPressed = [];
+
+var inputs = [];
+
+var inputId = 1;
+
+var canvas;
+var ctx;
+
+var socket = io.connect('localhost:8080');
+socket.on('game_update', function (data) {
+    if (inputs.length > 500) {
+        console.log(inputs);
+    }
+    var myPlayer = data.players[socket.id];
+    if (myPlayer !== undefined && player === undefined) {
+        player = {
+            x: myPlayer.x,
+            y: myPlayer.y,
+            vx: 0,
+            vy: 0
+        };
+        requestAnimationFrame(function () {
+            mainLoop(Date.now());
+        });
+    } else if (data.players.length !== 0) {
+        var inputProcessed = data.inputs.filter(function (input) {
+            return input.id === socket.id;
+        });
+        inputs = inputs.slice(inputProcessed.length);
+        var self = myPlayer;
+        player.x = self.x;
+        player.y = self.y;
+        var xAdded = 0;
+        for(var idx = 0; idx < inputs.length; idx++){
+            var input = inputs[idx];
+            player.x += input.vx * input.elapsedTime;
+            xAdded += input.vx * input.elapsedTime;
+            player.y += input.vy * input.elapsedTime;
+        }
+    }
+
+    if (otherPlayers === undefined) {
+        otherPlayers = data.players;
+        otherPlayers[socket.id] = undefined;
+    }
+});
+
+$(document).ready(function () {
+        canvas = $('canvas')[0];
+
+        ctx = canvas.getContext('2d');
+
+        $(document).on('keydown', function (ev) {
+            if (!keyPressed.includes(ev.which))
+                keyPressed.push(ev.which);
+        });
+
+        $(document).on('keyup', function (ev) {
+            var idx = keyPressed.indexOf(ev.which);
+            if (idx !== -1)
+                keyPressed.splice(idx, 1);
+        });
+    }
+);
+
+function mainLoop(time) {
+    var elapsedTime = (Date.now() - time) / 1000;
+
+    if (elapsedTime >= 14 / 1000) {
+        time = Date.now();
+        player.vx = 0;
+        player.vy = 0;
+        for(var idx = 0; idx < DIRECTIONS.length; idx++){
+            var direction = DIRECTIONS[idx];
+            if (keyPressed.includes(direction.code)) {
+                player.vx += direction.dir.x;
+                player.vy += direction.dir.y;
+            }
+        };
+
+        if (player.vx !== 0 || player.vy !== 0) {
+            player.x += player.vx * SPEED * elapsedTime;
+            player.y += player.vy * SPEED * elapsedTime;
+
+            var input = {elapsedTime: elapsedTime, inputId: inputId++, vx: player.vx, vy: player.vy};
+            inputs.push(input);
+            socket.emit('player_update', input);
+        }
+    }
+    drawAll();
+
+    requestAnimationFrame(function () {
+        mainLoop(time);
+    });
+}
+
+function drawAll() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(player.x, player.y, SQUARESIZE, SQUARESIZE);
+    for (var playerId in otherPlayers) {
+        if (otherPlayers.hasOwnProperty(playerId)) {
+            var currentPlayer = otherPlayers[playerId];
+            ctx.fillRect(currentPlayer.x, currentPlayer.y, SQUARESIZE, SQUARESIZE);
+        }
+    }
+}
